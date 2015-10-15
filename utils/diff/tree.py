6cad0feb4937784d066c248fae2a9cbe92b537db
raven.oscar <raven.oscar@gmail.com>
2015-10-15 16:27:59 +0300

    project tree builder

---
+++ utils/diff/tree.py
@@ -0,0 +1,11 @@
+import os
+
+def project_tree(project_path,name):
+	project_path = os.path.join(project_path,name)
+	if not os.path.exists(project_path):
+		os.makedirs(project_path)
+		for d in ['images/source','images/clipped','images/not_clipped','data/adv','data/reg','data/json']:
+			os.makedirs(os.path.join(project_path,d))
+
+if __name__ == "__main__":
+	project_tree('/home/raven/gen_projects/','horeca')
